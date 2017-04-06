9347437005be5d28fcdb26c7bdbafd9613052356
Neil Thomson <njt@fishlegs.co.uk>
2017-04-06 20:42:13 +0100

    Make Extras type an extension of ADACKvp

---
+++ src/ADAC/ExtrasKvp.java
@@ -0,0 +1,23 @@
+package ADAC;
+
+public class ExtrasKvp extends ADACKvp {
+
+	public static int LENGTH = ADACDictionary.valLength[ADACDictionary.PROGRAM_SPECIFIC];
+	
+	public ExtrasKvp(KvpListener listener, ADACKey key) {
+		super(listener, key);
+	}
+
+	@Override
+	public String getString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void read() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
